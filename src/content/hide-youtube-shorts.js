827d42ddc20fd062b1b5d8c6d112fa2e062f85c5
Imran Shaikh <imran@coderstea.in>
2026-02-21 18:25:10 +0530

    mutation added

--- src/content/hide-youtube-shorts.js
+++ src/content/hide-youtube-shorts.js
@@ -1,14 +1,31 @@
-// YouTube Shorts handling
 (function () {
-  // Redirect YouTube Shorts to regular video page (only if enabled)
-  const currentUrl = window.location.href;
-  if (currentUrl.includes("youtube.com/shorts")) {
-    chrome.storage.local.get("yt-short-to-video", (data) => {
-      if (data['yt-short-to-video']) {
-        const newUrl = currentUrl.replace("/shorts/", "/watch?v=");
-        console.log("Redirecting from Shorts to:", newUrl);
-        window.location.replace(newUrl);
-      }
-    });
-  }
+    redirectShorts();
 })();
+
+function redirectShorts() {
+    if (window.location.href.indexOf("youtube.com/shorts") > -1) {
+        chrome.storage.local.get("yt-short-to-video", (data) => {
+            if (data['yt-short-to-video']) {
+                window.location.replace(
+                    window.location.toString().replace("/shorts/", "/watch?v="),
+                );
+            }
+        });
+    }
+}
+
+window.onload = function () {
+    redirectShorts();
+    var bodyList = document.querySelector("body");
+    var observer = new MutationObserver(function (mutations) {
+        mutations.forEach((_mutation) => {
+            redirectShorts();
+        });
+    });
+    var config = {
+        childList: true,
+        subtree: true,
+    };
+    observer.observe(bodyList, config);
+};
+
